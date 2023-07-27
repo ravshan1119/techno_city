4a34de23c0aa91f3d0d82a70b3454c9247061bb7
ravshan1119 <allamurodovravshan38@gmail.com>
2023-07-27 15:43:55 +0500

    some changes

--- lib/ui/tab/tab_box.dart
+++ lib/ui/tab/tab_box.dart
@@ -1,32 +1,76 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:techno_city/providers/auth_provider.dart';
 
 class TabBox extends StatefulWidget {
-  const TabBox({super.key});
-
   @override
-  State<TabBox> createState() => _TabBoxState();
+  _TabBoxState createState() => _TabBoxState();
 }
 
 class _TabBoxState extends State<TabBox> {
+  int _page = 0;
+  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Center(
-            child: Text("tabBox"),
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 0,
+          height: 60.0,
+          items: <Widget>[
+            Icon(Icons.add, size: 30),
+            Icon(Icons.list, size: 30),
+            Icon(Icons.compare_arrows, size: 30),
+            Icon(Icons.call_split, size: 30),
+            Icon(Icons.perm_identity, size: 30),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.white,
+          backgroundColor: Colors.blueAccent,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 600),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          letIndexChange: (index) => true,
+        ),
+        body: Container(
+          color: Colors.blueAccent,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Center(
+                      child: Text("tabBox"),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          context.read<AuthProvider>().LogOutUser(context);
+                        },
+                        child: const Text("log out"))
+                  ],
+                ),
+                Text(_page.toString(), textScaleFactor: 10.0),
+                ElevatedButton(
+                  child: Text('Go To Page of index 1'),
+                  onPressed: () {
+                    final CurvedNavigationBarState? navBarState =
+                        _bottomNavigationKey.currentState;
+                    navBarState?.setPage(1);
+                  },
+                )
+              ],
+            ),
           ),
-          TextButton(
-              onPressed: () {
-                context.read<AuthProvider>().LogOutUser(context);
-              },
-              child: const Text("log out"))
-        ],
-      ),
-    );
+        ));
   }
 }
+
+
